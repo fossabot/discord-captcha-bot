c9a992e0f1d13c4373dac349d4d2100019216fd1
Alek Sasuni <VickTorriVIP@gmail.com>
2019-07-05 11:43:55 -0700

    cleaned up index
    no glitch support

--- index.js
+++ index.js
@@ -10,16 +10,3 @@ const client = new Client({
 client.run();
 client.login("YOUR TOKEN");
 
-/*
-const http = require('http');
-const express = require('express');
-const app = express();
-app.get("/", (request, response) => {
-    response.sendStatus(200);
-});
-
-app.listen(process.env.PORT);
-setInterval(() => {
-    http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
-}, 280000);
-*/
